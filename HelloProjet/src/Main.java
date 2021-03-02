3d1d5b24631a22c19c00b369be13cd2fc9035f0c
niuben <1099348621@qq.com>
2021-03-03 01:49:07 +0800

    dev分支Main.java新增方法

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");a
 		System.out.println("A目录的修改内容");
 		System.out.println("B目录修改内容");
+    }
+	
+	public void dev() {
+		System.out.println("dev分支新增内容");
     }
 }
